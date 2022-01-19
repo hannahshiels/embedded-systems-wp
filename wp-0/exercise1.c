a42b4916c495f97c935e7a8f070e8547d1fd0da9
Hannah Shiels <thebelfastflygon@gmail.com>
2022-01-19 13:05:32 +0100

    add wp-0

---
+++ wp-0/exercise1.c
@@ -0,0 +1,11 @@
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels
+// Work package 0
+// Exercise 1
+// Submission code: XXXXXX 
+#include <stdio.h>
+// Main function in the program, no program arguments supported
+
+void main(void) {
+    // Print a string to the compile 
+    printf("%s", "Hello World!\n");; 
+}
---
+++ wp-0/exercise2.c
@@ -0,0 +1,11 @@
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels
+// Work package 0
+// Exercise 2
+// Submission code: XXXXXX 
+#include <stdio.h>
+// Main function in the program, one program argument supported
+// Print message and program argument
+void main(int argc,char*argv[]) {
+    // Format and print a string 
+    printf("%s %s%s","Hello World! - I'm", argv[1], "!"); 
+}
