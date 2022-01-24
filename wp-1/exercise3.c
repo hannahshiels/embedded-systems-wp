3118d4b6148e5f12d7540d24da36e988774d87f2
Lucas Nordgren <gusnordglu@student.gu.se>
2022-01-24 19:44:41 +0100

    wp1 ex3,4,5 base

---
+++ wp-1/exercise3.c
@@ -0,0 +1,14 @@
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels
+// Work package 1
+// Exercise 3
+// Submission code: XXXXXX
+
+/**
+ * This program...
+ **/
+
+// Includes
+#include <stdio.h>  // fgets,
+#include <stdlib.h> // atoi,
+#include <string.h> // strlen,
+#include <ctype.h>  // toUpperCase,
---
+++ wp-1/exercise4.c
@@ -0,0 +1,14 @@
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels
+// Work package 1
+// Exercise 4
+// Submission code: XXXXXX
+
+/**
+ * This program...
+ **/
+
+// Includes
+#include <stdio.h>  // fgets,
+#include <stdlib.h> // atoi,
+#include <string.h> // strlen,
+#include <ctype.h>  // toUpperCase,
