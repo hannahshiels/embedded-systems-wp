5389badf845345941ade8860ba2c6bc4668c1c65
Lucas Nordgren <gusnordglu@student.gu.se>
2022-01-24 10:07:49 +0100

    move variables outside of main method

--- wp-1/exercise1.c
+++ wp-1/exercise1.c
@@ -4,38 +4,41 @@
 // Submission code: XXXXXX
 
 /**
- * This program reads in an integer number between 1 and 5 from the keyboard,
- * and prints out one of existing five sentences on the console depending on what number was entered.
- * 
- * The program continues to ask for a new number and exits if number isn't in the interval 1 to 5.
+ * This program:
+ * 1. Reads in an integer number between 1 and 5 from the keyboard.
+ * 2. Prints out one of five sentences on the console depending on input.
+ * 3. The program continues to ask for a new number if input is between 1 and 5
+ * 4. The program exits if input isn't a number or in the interval 1 to 5.
  **/
 
 // Includes
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 // Declare Functions
 int is_number(char *val);
 int is_num_between(int i, int start, int end);
 
+// Declare Constants
+const char *INPUT_MESSAGE = "\nPlease enter a value between 1-5: ";
+const char *END_MESSAGE = "\nThe program is shutting down...";
+const char *array[] = { // Define an array with messages
+    "Hehehe!",
+    "HAHAHA!",
+    "MWAHAHAHA!",
+    "MYAHAHAHAHAHAHA!!",
+    "FNGHAHAHAHAHAHAHAHAHAHAHAAHAFHS!!!!!!"};
+
+// Declare Variables
+char input[10];       // A char array for storing user input
+int isNumber = 1;     // boolean for storing if "inputAsInt" is a number
+int isNumBetween = 1; // boolean for storing if "inputAsInt" is between a given range
+
 // Main function in the program, one program argument supported with errors handled
 void main(void)
 {
-   // printf messages
-   const char *INPUT_MESSAGE = "\nPlease enter a value between 1-5: ";
-   const char *END_MESSAGE = "\nThe program is shutting down...";
-
-   // Define an array with messages
-   const char *array[] = {
-       "Hehehe!",
-       "HAHAHA!",
-       "MWAHAHAHA!",
-       "MYAHAHAHAHAHAHA!!",
-       "FNGHAHAHAHAHAHAHAHAHAHAHAAHAFHS!!!!!!"};
-
-   char input[10];       // A char array for storing user input
-   int inputAsInt;       // For storing converted input
-   int isNumber = 1;     // boolean for storing if "inputAsInt" is a number
-   int isNumBetween = 1; // boolean for storing if "inputAsInt" is between a given range
 
    // Run the while loop while the input is a number and while it is between a given range
    while (isNumber && isNumBetween)
@@ -46,16 +49,11 @@ void main(void)
       int inputAsInt = atoi(input);                    // Convert input to integer
       isNumber = is_number(input);                     // Store boolean
       isNumBetween = is_num_between(inputAsInt, 1, 5); // Store boolean
-      if (isNumber == 1)                               // Check if input is number
+      if (isNumber == 1 && isNumBetween == 1)          // Check if input is number && between a given range
       {
-         if (isNumBetween == 1) // Check if input is between a given range
-         {
-            int index = inputAsInt - 1;
-            printf("%s", array[index]); // Print the related char array based on inputed number
-         }
+         int index = inputAsInt - 1; // EX: input of 1 will print index 0 in array
+         printf("%s", array[index]); // Print the related char array based on inputed number
       }
-
-      // free(input); // free input from memory
    }
 
    printf("%s", END_MESSAGE); // Print end-of-program message
@@ -65,7 +63,7 @@ void main(void)
  * This function checks if a value(val) is a number
  * RETURN 1(true) if val is a number, else false
  **/
-int is_number(char *val)
+int is_number(char *val) // Why can't we say "val" instead?
 {
    int isDigit = 0; // boolean
 
@@ -73,7 +71,7 @@ int is_number(char *val)
    {
       isDigit = isdigit(val[i]); // Check if current index is a number
 
-      if (isDigit == 0)
+      if (0 == isDigit)
          return 0; // return 0 if detecting non-integer value
    }
 
