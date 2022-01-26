acc74ad0a0a903636dc2ad31d21b884ec2c7a78f
Lucas Nordgren <gusnordglu@student.gu.se>
2022-01-26 14:16:11 +0100

    exercise 5 now prints correctly

--- wp-1/exerc_1_5.c
+++ wp-1/exerc_1_5.c
@@ -22,6 +22,10 @@
 // Defines
 #define MAX 100      // Defines the maximum number of the values in the table
 #define MAXNUMBER 20 // Defines the maximum value of random numbers
+#define LOWNUMBER 1  // Defines the maximum value of random numbers
+
+// Variables
+const char *X = "x";
 
 // ------ Function declarations   ----------
 
@@ -43,8 +47,8 @@ int *create_random(int *tab)
 {
    for (size_t i = 0; i < MAX; i++)
    {
-      int randNum = rand() % (MAX + 1); // generates a number between 0-100
-      tab[i] = randNum;                 // set the current index in tab array to randNum
+      int randNum = (rand() % (MAXNUMBER - LOWNUMBER + 1) + LOWNUMBER); // generates a number between 0-100
+      tab[i] = randNum;                                                 // set the current index in tab array to randNum
    }
 
    return tab;
@@ -52,32 +56,49 @@ int *create_random(int *tab)
 
 void count_frequency(int *tab, int *freq)
 {
-   // int table[MAX];
-   int freqSize = 0;
+   int tempTable[MAX];
+   for (size_t i = 0; i < MAX; i++)
+   {
+      tempTable[i] = tab[i];
+   }
 
+   // from 1-100
    for (size_t i = 0; i < MAX + 1; i++)
    {
-      int currTab = tab[i];
+      int tabI = tab[i];
+      int count = 1; // set count for number frequency; since there will be a number in every index, always set to
 
-      for (size_t j = 0; j <= freqSize; j++)
+      // Check tab array from 1-100
+      for (size_t j = i + 1; j <= MAX; j++)
       {
-         int currFreq = freq[j];
+         int tabJ = tab[j];
 
-         if (currFreq == currTab)
+         if (tabI == tabJ && tempTable[i] != 0)
          {
-            freq[j]++;
-         }
-         else if (j == (freqSize - 1))
-         {
-            freq[freqSize] = tab[i];
-            freqSize++;
+            count++;
+
+            tempTable[j] = 0;
          }
       }
+
+      freq[i] = count;
    }
 }
 
 void draw_histogram(int *freq)
 {
+
+   for (size_t i = 0; i <= MAXNUMBER; i++)
+   {
+      if (freq[i] != 0)
+      {
+         printf("\n%d: ", i);
+         for (size_t j = 0; j < freq[i]; j++)
+         {
+            printf("%s", X);
+         }
+      }
+   }
 }
 
 // ------ Main   --------------------------
@@ -90,21 +111,13 @@ int main(void)
 {
    int table[MAX], n;
    int frequency[MAXNUMBER];
+
+   for (size_t i = 0; i < MAXNUMBER; i++)
+      frequency[i] = -1; // reset frequency array
+
    srand(time(0)); // use the current time as seed for generating random numbers.
 
    create_random(table);
    count_frequency(table, frequency);
    draw_histogram(frequency);
-
-   int i = 0;
-   while (frequency[i] != NULL)
-   {
-      printf("%d\n", frequency[i]);
-      i++;
-   }
-
-   for (size_t i = 0; i < MAX; i++)
-   {
-      printf("%d\n", table[i]);
-   }
 }
