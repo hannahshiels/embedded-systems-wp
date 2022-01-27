027c48288b823bf316716d612a4f38849bd1b592
Lucas Nordgren <gusnordglu@student.gu.se>
2022-01-27 21:53:34 +0100

    Add -h; Formatting...

--- wp-1/exerc_1_4_dec_to_bin.c
+++ wp-1/exerc_1_4_dec_to_bin.c
@@ -3,7 +3,6 @@
 // Exercise 4
 // Submission code: XXXXXX
 
-
 //You should write two programs, which convert between numbers in different format. You should make sure that these
 //programs can be executed in a pipeline.
 
@@ -15,15 +14,13 @@
 #include <string.h>
 #include <ctype.h>
 
-
 /**
  * Define Section
  */
-#define Max_8bit_Num 255   //Largest decimal number that had 8 digits as a binary number
-#define Max_16bit_Num 2147483647   //Largest number existing as lon in C compiler
-#define Short_Length_Binary 8   //The number of the digits displayed to user while their number is less than maximum 8 bit number
-#define Long_length_Binary 16   //The number of the digits displayed to user when their number is less than maximum 16 bit number
-
+#define Max_8bit_Num 255         //Largest decimal number that had 8 digits as a binary number
+#define Max_16bit_Num 2147483647 //Largest number existing as lon in C compiler
+#define Short_Length_Binary 8    //The number of the digits displayed to user while their number is less than maximum 8 bit number
+#define Long_length_Binary 16    //The number of the digits displayed to user when their number is less than maximum 16 bit number
 
 //This method converts a decimal number into binary format and displays the results using the number and the array length
 //needed to save and show the results
@@ -33,15 +30,15 @@ void binConverter(long input, int arrayLength);
 * first code: Convert a decimal number to binary format
 */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     //Variable initiation
-    unsigned long long longChecker;   //To check if the number is in the allowable range of long type in C compiler
-    const char *errMsg = "The number you entered is too big for this compiler. Try again later..";   //Occurs when the argument magnitude is higher than maximum 16 bit
-    const char *errMsg2 = "The value in the argument is not a number";   //Occurs if the argument is a string
-    int intChecker; //To check if the argument is a number
-    long int input;   //the in format entered as argument
-
+    unsigned long long longChecker;                                                                 //To check if the number is in the allowable range of long type in C compiler
+    const char *errMsg = "The number you entered is too big for this compiler. Try again later..";  //Occurs when the argument magnitude is higher than maximum 16 bit
+    const char *errMsg2 = "The value in the argument is not a number";                              //Occurs if the argument is a string
+    const char *helpMsg = "Enter a decimal value and receive the equivalent binary version of it."; //will be displayed if the user is seeking help
+    int intChecker;                                                                                 //To check if the argument is a number
+    long int input;                                                                                 //the in format entered as argument
 
     //Body
 
@@ -60,45 +57,53 @@ int main(int argc, char* argv[])
     //wants us to use long for input so that is why there are various variables defined.
     intChecker = *argv[1];
 
-    //Check for errors
-
-    if (longChecker > Max_16bit_Num) {   //if the number entered as the argument is more than the maximum long in C compiler
+    if (strcmp(argv[1], "-h") == 0) //if the user input is '-h' and user seeks help
+    {
+        //the help message is displayed
+        printf("%s", helpMsg);
+        //exit with code 2
+        return 2;
+    }
+    //Check for errors:
+    else if (longChecker > Max_16bit_Num)
+    { //if the number entered as the argument is more than the maximum long in C compiler
         //display the relevant error to the user
         printf("%s", errMsg);
         //exit with code number 2
         return 2;
-
-    } else if (isdigit(intChecker) == 0)   //if the argument contains non digit values
+    }
+    //Check for errors:
+    else if (isdigit(intChecker) == 0) //if the argument contains non digit values
     {
         //display the relevant error
         printf("%s", errMsg2);
         //exit with code 2
         return 2;
-
-    } else {   //if the argument is a regular decimal number
+    }
+    else
+    { //if the argument is a regular decimal number
 
         //Check the magnitude of the argument
-        if (input <= Max_8bit_Num)   //if the number's binary form is less than 8digits
+        if (input <= Max_8bit_Num) //if the number's binary form is less than 8digits
         {
             //convert the number displaying an 8 digit binary
-            binConverter ( input, Short_Length_Binary);
-
-        } else if ( input <= Max_16bit_Num && input > Max_8bit_Num)  //if the number's binary form is more than 8 digits
+            binConverter(input, Short_Length_Binary);
+        }
+        else if (input <= Max_16bit_Num && input > Max_8bit_Num) //if the number's binary form is more than 8 digits
         {
             //convert the number displaying an 16 digit binary
-            binConverter ( input, Long_length_Binary);
+            binConverter(input, Long_length_Binary);
         }
         //exit with code 0
         return 0;
     }
-
 }
 
-
-void binConverter(long input, int arrayLength) {
+void binConverter(long input, int arrayLength)
+{
     //variable declaration
-    int i;   //used in loops to keep the track of indices
-    int output[arrayLength];   //generate an array with the length needed to save the binary values
+    int i;                   //used in loops to keep the track of indices
+    int output[arrayLength]; //generate an array with the length needed to save the binary values
 
     //empty the memory allocated to the output array by iterating over the array replace previous values with 0
     for (i = 0; i < arrayLength; i++)
@@ -109,7 +114,7 @@ void binConverter(long input, int arrayLength) {
 
     //get the binary equivalent to the input by saving the remainders of the division of input over 2.
     //The loop continues until the input value is so low that the result of input/2 is less than 1.
-    for(i = 0; input > 0; i++)
+    for (i = 0; input > 0; i++)
     {
         //get the remainder value of the number divided by 2 and store it in the output array
         output[i] = input % 2;
@@ -117,13 +122,9 @@ void binConverter(long input, int arrayLength) {
         input = input / 2;
     }
     //print out binary format of the input by starting from the last element in the output array
-    for(i = (arrayLength - 1); i >= 0; i--)
+    for (i = (arrayLength - 1); i >= 0; i--)
     {
         //prints out the values
-        printf("%d",output[i]);
+        printf("%d", output[i]);
     }
-
 }
-
-
-
