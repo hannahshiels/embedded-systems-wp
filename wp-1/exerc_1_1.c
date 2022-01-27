5d5ebdcdc030ac72effe88b51c66408c85c9fdde
Hannah Shiels <thebelfastflygon@gmail.com>
2022-01-27 16:01:14 +0100

    back to if statements

--- wp-1/exerc_1_1.c
+++ wp-1/exerc_1_1.c
@@ -9,14 +9,14 @@
 
 #include <stdio.h>
 
-
-// init variables 
+// init variables, messages to display to the user
 const char prompt[] = "Enter an integer between 1 and 5: \n";
 const char firstSentence[] = "Hello, my name is Hannah! \n";
 const char secondSentence[] = "Hello, my name is Lucas! \n";
 const char thirdSentence[] = "Hello, my name is Maryam! \n";
 const char fourthSentence[] = "We are in the course DIT632! \n";
 const char fifthSentence[] = "Our teacher is Miroslaw Staron! \n";
+const char exitMessage[] = "Not within range. Exiting....";
 
 void main(void)
 {
@@ -26,45 +26,37 @@ void main(void)
     // stop loop if cont equals 1
     while (cont != 1)
     {
-        printf("%s", prompt);  // ask for number
-        scanf("%d", &num); // input  a number
+        printf("%s", prompt); // ask for number
+        scanf("%d", &num);    // input  a number
 
         // stop the program if number is out of range
         if (num < 1 || num > 5)
         {
-            cont = 1; // stop loop
-            printf("Not within range. Exiting....");
+            cont = 1;                  // stop loop
+            printf("%s", exitMessage); // print exit message
         }
         else // usual case, number is within range
         {
             // print a different sentence based on the number
-            switch (num)
+            if (num == 1) // if 1 is entered
             {
-            case 1:
-            {
-                printf("%s", firstSentence);
-                break;
+                printf("%s", firstSentence); // if 1 is entered show corresponding sentence
             }
-            case 2:
+            else if (num == 2) // if 2 is entered
             {
-                printf("%s", secondSentence);
-                break;
+                printf("%s", secondSentence); // if 2 is entered show corresponding sentencce
             }
-            case 3:
+            else if (num == 3) // if 3 is entered
             {
-                printf("%s", thirdSentence);
-                break;
+                printf("%s", thirdSentence); // if 3 is entered show corresponding sentence
             }
-            case 4:
+            else if (num == 4) // if 4 is entered
             {
-                printf("%s", fourthSentence);
-                break;
+                printf("%s", fourthSentence); // if 4 is entered show corresponding sentence
             }
-            case 5:
+            else if (num == 5) // if 5 is entered
             {
-                printf("%s", fifthSentence);
-                break;
-            }
+                printf("%s", fifthSentence); // if 5 is entered show corresponding sentence
             }
         }
     }
