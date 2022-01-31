8a95df000a4358c0ea57966fa1bde4854e740360
Lucas Nordgren <gusnordglu@student.gu.se>
2022-01-31 13:26:08 +0100

    Add template for code

---
+++ template.c
@@ -0,0 +1,43 @@
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels: Group 6 2022
+// Work package x
+// Exercise x
+// Submission code: xxxxxx
+
+/**
+ * This program ...
+ **/
+
+// ------ Includes   ----------
+
+#include <stdio.h>  // fgets,
+#include <stdlib.h> // atoi,
+#include <string.h> // strlen,
+#include <ctype.h>  // toUpperCase,
+#include <time.h>   // time
+
+// ------ Defines   ----------
+
+#define EXAMPLE = 911 // Example define
+
+// ------ Variables   ----------
+
+int exampleInt = 0;
+
+// ------ Function declarations   ----------
+
+// This function generates a set of random numbers
+void example_function();
+
+// ------ Main   --------------------------
+// The main entry point for the program
+int main(void)
+{
+   // ...
+}
+
+// ------ Function definitions   ----------
+
+void example_function()
+{
+   // ...
+}
