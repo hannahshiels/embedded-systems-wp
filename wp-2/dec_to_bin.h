2cade689c19b30c4ce238f4ce3770c9c0448b072
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-02-02 09:32:04 +0100

    exerc 4 part a is mostly done

---
+++ wp-2/dec_to_bin.h
@@ -0,0 +1,30 @@
+void binConverter(long input, int arrayLength, char* outPut)
+{
+    //variable declaration
+    int i;   //used in loops to keep the track of indices
+    long tempOutput[arrayLength]; //generate an array with the length needed to save the binary values
+
+    //empty the memory allocated to the tempOutput array by iterating over the array replace previous values with 0
+    for (i = 0; i < arrayLength; i++)
+    {
+        //place zero instead of the possible existing values
+        tempOutput[i] = 0;
+    }
+
+    //get the binary equivalent to the input by saving the remainders of the division of input over 2.
+    //The loop continues until the input value is so low that the result of input/2 is less than 1.
+    for (i = 0; input > 0; i++)
+    {
+        //get the remainder value of the number divided by 2 and store it in the tempOutput array
+        tempOutput[i] = input % 2;
+        //next step
+        input = input / 2;
+    }
+    //print out binary format of the input by starting from the last element in the tempOutput array
+    for (i = (arrayLength - 1); i >= 0; i--)
+    {
+        //prints out the values
+        //printf("\n%ld", tempOutput[i]);
+        outPut[arrayLength - i - 1] = (char) tempOutput[i];
+    }
+}
--- wp-2/exerc_2_4.c
+++ wp-2/exerc_2_4.c
@@ -1,29 +1,85 @@
-// (C) Maryam Esmaeili Darestani, Lucas Nordgren, Hannah Shiels: Group 6 2022
+// (C) Maryam Esmaeli Darestani, Lucas Nordgren, Hannah Shiels: Group 6 2022
 // Work package 2
 // Exercise 4
-// Submission code:
-
-
+// Submission code: xxxxxx
 
 /**
-* Include section
-*/
+ * This program ...
+ **/
+
+// ------ Includes   ----------
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // strlen
+#include "dec_to_bin.h"
+
+// ------ Defines   ----------
+
+#define LENGTH_ERROR "There must be 8bits in a byte! Try again..." //will be displayed in case of invalid argument
+#define NUMBER_OF_BITS 8   //number of bits in a single byte
+#define ENGINE_BITS_SHIFT 7   //The bits allocated to engine in the memory will shift 7 times to the right
+#define GEAR_BITS_SHIFT 4   //The bits allocated to gear in the memory will shift 4 times to the right
+#define KEY_BITS_SHIFT 2    //The bits allocated to keys in the memory will shift 2 times to the right
+#define BRAKE1_BITS_SHIFT 1   //The bits allocated to break2 in the memory will shift 1 times to the right
+
+// ------ Variables   ----------
 
 
-/**
-* Main body
-*/
+// ------ Function declarations   ----------
 
-int main (void)
+int lengthChecker(char *argument);   //checks the length of the argument
+
+// ------ Main   --------------------------
+// The main entry point for the program
+int main(int argc, char *argv[])
 {
-    char b[] = "maryam!";
-    char *example = &b;
-    int a = 125;
-    int *p = &a;
-    printf("%p\n", *example);
-    printf("%p\n%p\n", *p, &a);
-    //printf("%s\n", b);
+
+    //variable declaration:
+    unsigned char car;   //the values in the argument are saved in this variable as bits
+    unsigned int engine_on;
+    unsigned int gear_pos;
+    unsigned int key_pos;
+    unsigned int brake1;
+    unsigned int brake2;
+    char binCar[NUMBER_OF_BITS];
+
+    //error handling
+    if (lengthChecker(argv[1]) == 0)
+    {
+        printf(LENGTH_ERROR);
+        return 1;
+    }
+    //TODO complete the err handling later
+
+    //assign value to the car attributes
+    engine_on = argv[1][0] - '0';   //convert the value from char to int
+    gear_pos = argv[1][1] - '0';   //convert the value from char to int
+    key_pos = argv[1][2] - '0';   //convert the value from char to int
+    brake1 = argv[1][3] - '0';   //convert the value from char to int
+    brake2 = argv[1][4] - '0';   //convert the value from char to int
+
+    car = ((engine_on << ENGINE_BITS_SHIFT) | (gear_pos << GEAR_BITS_SHIFT) | (key_pos << KEY_BITS_SHIFT) | (brake1 << BRAKE1_BITS_SHIFT) | brake2);
+
+    binConverter((long)car, 8, binCar);
+
+    for (int i = 0;  i < sizeof (binCar); i++)
+    {
+        printf("%d", binCar[i]);
+    }
+
+
+    printf("\nThe size of the car is: %llu", sizeof(car));
+    printf("\n%02lX", (long)car);
+}
+
+// ------ Function definitions   ----------
+
+int lengthChecker(char *argument) {
+    //Get the length of the argument
+    if (strlen(argument) == 5)   //If the length is equal to 6 considering the last character to be /0
+    {
+        return 1;
+    }
     return 0;
 }
