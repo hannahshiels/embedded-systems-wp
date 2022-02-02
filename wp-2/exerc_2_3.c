ca8357f3184e68520c2601952fdc506edd9883d2
hannahshiels <thebelfastflygon@gmail.com>
2022-02-02 20:43:31 +0100

    options are done probably
    needs to be cleaned up :(

--- wp-2/exerc_2_3.c
+++ wp-2/exerc_2_3.c
@@ -56,7 +56,7 @@ After entered the choice the program executes the task and returns to the menu f
 
 // ------ Variables etc   ----------
 
-const char *MENU = "\nSelect an option from 1 -5. \n\n1. Create a new and delete the old file. \n2. Add a new person to the file. \n3. Search for a person in the file. \n4. Print out all in the file. \n5. Exit the program. ";
+const char *MENU = "\nSelect an option from 1 - 5. \n\n1. Create a new and delete the old file. \n2. Add a new person to the file. \n3. Search for a person in the file. \n4. Print out all in the file. \n5. Exit the program. ";
 const char *fileName = "binaryFile.bin";
 
 // ------ Structs   ----------
@@ -81,18 +81,18 @@ void append_file(PERSON *inrecord);    // appends a new person to the file
 // ------ Main   --------------------------
 int main(void)
 {
-    PERSON *ppost, person;
-    ppost = &person;
+    PERSON ppost, *person;
+    person = &ppost;
     // only using these right now to add as dummy record when option 1 is entered
-    scanf("%s", ppost->firstname);
-    scanf("%s", ppost->famname);
-    scanf("%s", ppost->pers_number);
+    printf("%s", "\nCreate a dummy record: ");
+    printf("%s", "\nEnter first name: ");
+    scanf("%s", person->firstname);
+    printf("%s", "Enter last name: ");
+    scanf("%s", person->famname);
+    printf("%s", "Enter personal number: ");
+    scanf("%s", person->pers_number);
 
-    //  fgets(ppost->firstname, 20, stdin);
-    //  fgets(ppost->famname, 20, stdin);
-    //  fgets(ppost->pers_number, 13, stdin);
-
-    start(ppost);
+    start(person);
     return (0);
 }
 
@@ -109,13 +109,11 @@ void start(PERSON *inrecord)
         {
         case 1:
         {
-            printf("%s", "option 1");
             write_new_file(inrecord);
             break;
         };
         case 2:
         {
-            printf("%s", "option 2");
             PERSON person = input_record();
             PERSON *personP = &person;
             append_file(personP);
@@ -123,12 +121,14 @@ void start(PERSON *inrecord)
         };
         case 3:
         {
-            printf("%s", "option 3");
+            char str[] = "";
+            printf("%s","\nEnter first name to search by: ");
+            scanf("%s", &str);
+            search_by_firstname(str);
             break;
         };
         case 4:
         {
-            printf("%s", "option 4");
             printfile();
             break;
         };
@@ -162,7 +162,8 @@ void write_new_file(PERSON *inrecord) // Creates a file and writes the first rec
     FILE *file = fopen(fileName, "wb");
     if (file != NULL)
     {
-        fwrite(inrecord, sizeof(PERSON), 1, file);
+        fwrite(inrecord, sizeof(PERSON) - 1, 1, file);
+        fwrite("\n", sizeof(char), 1, file);
     }
     else
     {
@@ -178,26 +179,59 @@ void printfile(void) // Prints out all persons in the file
     FILE *file = fopen(fileName, "rb");
     if (file != NULL)
     {
+        PERSON person;
+        while (fread(&person, sizeof(PERSON), 1, file))
+        {
+            printf("%s", "\n------------------------------------------------------");
+            printf("\nName: %s %s | Personal Number: %s", person.firstname, person.famname, person.pers_number);
+        }
     }
     else
     {
-        printf("%s", "\n Something went wrong...");
+        printf("%s", "\nSomething went wrong...");
     }
-    printf("%s", "\n");
     fclose(file);
 }
 
 // TODO
 void search_by_firstname(char *name) // Prints out the person if in list
 {
+    FILE *file = fopen(fileName, "rb");
+    int nr = 0;
+    if (file != NULL)
+    {
+        PERSON person;
+        while (fread(&person, sizeof(PERSON), 1, file))
+        {
+            if (strcmp(person.firstname, name) == 0)
+            {
+                printf("\nName: %s %s | Personal Number: %s", person.firstname, person.famname, person.pers_number);
+                nr++;
+            }
+            if (strcmp(person.famname, name) == 0)
+            {
+                printf("\nName: %s %s | Personal Number: %s", person.firstname, person.famname, person.pers_number);
+                nr++;
+            }
+        }
+        if (nr == 0)
+        {
+            printf("%s", "\nNo matches found!");
+        }
+    }
+    else
+    {
+        printf("%s", "Something went wrong.");
+    }
+    fclose(file);
 }
 void append_file(PERSON *inrecord) // appends a new person to the file
 {
     FILE *file = fopen(fileName, "ab");
     if (file != NULL)
     {
-        fwrite("\n", sizeof(char), 1, file);
-        fwrite(inrecord, sizeof(PERSON), 1, file);
+        fwrite(inrecord, sizeof(PERSON) - 1, 1, file);
+        fwrite("\n", 1, 1, file);
     }
     else
     {
