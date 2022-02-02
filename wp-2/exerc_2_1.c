d8dbc5f2815df5a0b6d85a519fb3562cfd75bcec
Lucas Nordgren <gusnordglu@student.gu.se>
2022-02-02 12:57:49 +0100

    Refactor; add end character 'x'

--- wp-2/exerc_2_1.c
+++ wp-2/exerc_2_1.c
@@ -30,17 +30,28 @@
 
 // ------ Defines   ----------
 
+// robot related values
 #define MAX_INPUT_POS_VAL 99     // max value for x,y
 #define MIN_INPUT_POS_VAL 0      // min value for x,y
 #define MAX_INPUT_POS 6          // max input for x,y; '99 99' + end char
 #define MAX_INPUT_DIR 100        // max input for 'm' and 't'
-#define INPUT_MESSAGE_1 "\nInput the Robots starting position(x y between 0-99): " // Input message 1
-#define INPUT_MESSAGE_2 "\nInput a sequence of 'm' and 't' (Ex 'mmmt'): "          // Input message 3
-#define ERROR_MESSAGE "\nYour input was not correct. Input -h for help"            // Input message 2
-#define HELP_MESSAGE "\nPosition: For the position you have to input two numbers between 0-99, seperated by a space" // Input message 4
-#define FINISH_MESSAGE "\nThe Robots new location is: "                // Input message 5
+// Printf messages:
+#define INPUT_MESSAGE_1 "\nInput the Robots starting position('x y' between 0-99, or '-h' for help): " // Input message 1
+#define INPUT_MESSAGE_2 "\nInput a sequence of 'm' and 't' (Ex 'mmmt', or '-h' for help): "            // Input message 2
+#define ERROR_MESSAGE "\nYour input was not correct. Input -h for help"            // ERROR message
+#define FINISH_MESSAGE "\nThe Robots location is: "                                // Robots position message
+// Help messages:
+#define HELP_MESSAGE_1 "\nPosition: For the position you have to input two numbers between 0-99, seperated by a space."
+#define HELP_MESSAGE_2 "\nMove & Turn: Move and turn by supplying any amount or combination of 'm' and 't'."
+#define HELP_MESSAGE_3 "\nExit: Leave program by inputting 'x'.\n"
+// input characters
+#define END_CHARACTER "x\n"
+#define HELP_CHARACTER "-h\n"
 
 // ------ Variables etc   ----------
+
+// to check if end of program; is set to false on specific user END_CHARACTER input
+int eop = 1;
 
 // defines possible directions
 enum DIRECTION {
@@ -61,14 +72,20 @@ typedef struct {
 
 // ------ Function declarations   ----------
 
-// This function moves the robot between 0-99 by changing its x and y values
+// This method handles user input, and handles the robots starting position
+int handlePositionInput(ROBOT *robot);
+
+// This method handles user input, and handles the robots direction and heading(turning and moving forward)
+int handleDirectionInput(ROBOT *robot);
+
+// This function moves the robot 1 time forward, by changing its x and y values (changed based on robots direction)
 void move(ROBOT *robot);
 
 // This function turns the robot 90 degrees
 void turn(ROBOT *robot);
 
 // This function prints input message, and gets user input
-void getInput(char *inputMessage, char *input, int maxInput);
+int getInput(char *inputMessage, char *input, int maxInput);
 
 // This function enables printf in Clion while debugging
 // https://stackoverflow.com/questions/47776094/clion-wont-show-output-in-debug
@@ -76,90 +93,23 @@ void enablePrintf();
 
 // ------ Main   --------------------------
 // The main entry point for the program
-// TODO: Refactor, Error handling
+// TODO: Refactor, Error handling, Make methods return true,false instead
 int main(int argc, char *argv[]) {
-   char dirInput[MAX_INPUT_DIR];       // input for direction
-   char startPosInput[MAX_INPUT_POS];  // input for starting position
-   int correctPosInput = 0;            // boolean for correct/false input
    ROBOT robot;                        // Create a robot
+   enablePrintf();                     // enable console logs when debugging
 
-   enablePrintf(); // enable console logs when debugging
+   // main loop runs while eop is true
+   while (eop) {
 
-   // main loop runs while no EOF
-   while (!feof(stdin)) {
+      // 1. manage robot starting position, end if function returns 0
+      if (handlePositionInput(&robot) == 0)
+         return 0;
 
-      // runs while user inputs wrong input for starting position
-      while (!correctPosInput) {
-         int loop = 0; // set loop to 0
+      // 2. manage robots movement and turning, end if function returns 0
+      if (handleDirectionInput(&robot) == 0)
+         return 0;
 
-         // Get input for start pos
-         getInput(INPUT_MESSAGE_1, startPosInput, MAX_INPUT_POS);
-
-         // get the first string from input
-         char *splitString = strtok(startPosInput, " ");
-
-         // loop through the string to extract all other strings
-         while (splitString != NULL) {
-            char *pEnd;    // ...
-            int stringAsInt = strtol(splitString, &pEnd, 10);
-
-            // set first string to x pos, then set y, only if value is between 0-99
-            if (loop == 0 && stringAsInt >= MIN_INPUT_POS_VAL && stringAsInt <= MAX_INPUT_POS_VAL) {
-               // convert input to int and store in x
-               robot.xpos = stringAsInt;
-            } else if (stringAsInt >= MIN_INPUT_POS_VAL && stringAsInt <= MAX_INPUT_POS_VAL) {
-               // convert input to int and store in y
-               robot.ypos = stringAsInt;
-            }
-
-            // grab the next string and set to splitString
-            splitString = strtok(NULL, " ");
-
-            // increase loop counter
-            loop++;
-         }
-
-         char const *help = "-h\n"; // help string
-         // if the user has supplied two strings seperated by a space; assumes they are
-         if (loop == 2) {
-            correctPosInput = 1;       // user has correct amount of input
-            // if the user input is '-h' and user seeks help, print help message
-         } else if (strcmp(startPosInput, help) == 0) {
-            printf("%s", HELP_MESSAGE);
-            // else, print error
-         } else {
-            printf("%s", ERROR_MESSAGE);
-         }
-      }
-
-      // get input for direction
-      getInput(INPUT_MESSAGE_2, dirInput, MAX_INPUT_DIR);
-
-      const char *pDirInput = dirInput; // create a pointer to input
-      int loop = 0; // for getting character in input
-
-      // Loop through the characters in "input" until "\0"
-      // Comparisons work based on character decimal codes
-      while (*pDirInput != '\0') {
-         char *currentChar;                  // store current character in input
-         currentChar = malloc(2);       // allocate memory
-         currentChar[0] = '\0';              // set to empty
-         strncat(currentChar, pDirInput, 1);
-
-         // If the character is 'm'
-         if (strcmp(currentChar, "m") == 0) {
-            move(&robot); // move
-            // if the character is 't'
-         } else if (strcmp(currentChar, "t") == 0) {
-            turn(&robot); // turn
-         }
-
-         free(currentChar); // free from memory
-
-         pDirInput++; // Move the pointer forward
-      }
-
-      // print robots final position
+      // 3. print robots final position
       printf("%sX = %d; Y = %d", FINISH_MESSAGE, robot.xpos, robot.ypos);
 
    }
@@ -169,8 +119,85 @@ int main(int argc, char *argv[]) {
 
 // ------ Function definitions   ----------
 
-void move(ROBOT *robot) {
+int handlePositionInput(ROBOT *robot) {
+   char startPosInput[MAX_INPUT_POS];  // input for starting position
+   int correctPosInput = 0;            // boolean for correct/false input
 
+   // runs while user inputs wrong input for starting position
+   while (!correctPosInput) {
+      int loop = 0; // set loop to 0
+
+      // Get input for start pos
+      if (getInput(INPUT_MESSAGE_1, startPosInput, MAX_INPUT_POS) == 0)
+         return 0;
+
+      // get the first string from input
+      char *splitString = strtok(startPosInput, " ");
+
+      // loop through the string to extract all other strings
+      while (splitString != NULL) {
+         char *pEnd;    // ...
+         int stringAsInt = strtol(splitString, &pEnd, 10);
+
+         // set first string to x pos, then set y, only if value is between 0-99
+         if (loop == 0 && stringAsInt >= MIN_INPUT_POS_VAL && stringAsInt <= MAX_INPUT_POS_VAL) {
+            // convert input to int and store in x
+            robot->xpos = stringAsInt;
+         } else if (stringAsInt >= MIN_INPUT_POS_VAL && stringAsInt <= MAX_INPUT_POS_VAL) {
+            // convert input to int and store in y
+            robot->ypos = stringAsInt;
+         }
+
+         // grab the next string and set to splitString
+         splitString = strtok(NULL, " ");
+
+         // increase loop counter
+         loop++;
+      }
+
+      // if the user has supplied two strings seperated by a space;
+      if (loop == 2) {
+         correctPosInput = 1;       // user has correct amount of input
+      }
+   }
+
+   return 1;
+}
+
+int handleDirectionInput(ROBOT *robot) {
+   char dirInput[MAX_INPUT_DIR];       // input for direction
+   // create a pointer to dirInput
+   const char *pDirInput = dirInput;
+
+   // get input for direction
+   if (getInput(INPUT_MESSAGE_2, dirInput, MAX_INPUT_DIR) == 0)
+      return 0;
+
+   // Loop through the characters in "input" until "\0"
+   // Comparisons work based on character decimal codes
+   while (*pDirInput != '\0') {
+      char *currentChar;                  // store current character in input
+      currentChar = malloc(2);       // allocate memory
+      currentChar[0] = '\0';              // set to empty
+      strncat(currentChar, pDirInput, 1);
+
+      // If the character is 'm'
+      if (strcmp(currentChar, "m") == 0) {
+         move(robot); // move
+         // if the character is 't'
+      } else if (strcmp(currentChar, "t") == 0) {
+         turn(robot); // turn
+      }
+
+      free(currentChar); // free from memory
+
+      pDirInput++; // Move the pointer forward
+   }
+
+   return 1;
+}
+
+void move(ROBOT *robot) {
    // based on robots direction, and if value is between 0-99, increase x and y
    if (robot->dir == 0) {                    // N
       robot->ypos++; // increase pos
@@ -188,15 +215,25 @@ void turn(ROBOT *robot) {
    robot->dir = ((robot->dir) + 1) % 4;
 }
 
-void getInput(char *inputMessage, char *input, const int maxInput) {
+int getInput(char *inputMessage, char *input, const int maxInput) {
    // print input message
    printf("%s", inputMessage);
 
    // get user input
    fgets(input, maxInput, stdin);
 
+   // if 'x' is inputted, set main loop boolean to false
+   if (strcmp(input, END_CHARACTER) == 0)
+      return 0;
+
+   // if '-h' is inputted, print help messages
+   if (strcmp(input, HELP_CHARACTER) == 0)
+      printf("%s%s%s", HELP_MESSAGE_1, HELP_MESSAGE_2, HELP_MESSAGE_3);
+
    // flush stdin
    fflush(stdin);
+
+   return 1;
 }
 
 void enablePrintf() {
