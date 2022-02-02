e4509079a290c358e0225115b8b0a45cc5a934e9
Hannah Shiels <thebelfastflygon@gmail.com>
2022-02-02 11:52:45 +0100

    finish exercise 2

--- wp-2/exerc_2_2.c
+++ wp-2/exerc_2_2.c
@@ -50,8 +50,8 @@ int main(int argc, char *argv[])
     }
 
     head = add_first(head, 101); // insert new node and set to new head
-    act_post = head; // reset pointer to head
-    nr = 0; // reset number
+    act_post = head;             // reset pointer to head
+    nr = 0;                      // reset number
 
     while (act_post != NULL)
     {
@@ -76,32 +76,35 @@ Node *random_list(void)
 {
     // init variables for the loop
     int nr, i = 0;
-    Node *head, *old, *item; 
+    Node *head, *old, *item;
 
     head = malloc(sizeof(Node)); // allocate memory
     old = malloc(sizeof(Node));  // allocate memory
 
     // create head
     head->number = get_random_number(); // assign number to head
-    head->prev = NULL; // init prev pointer
-    head->next = NULL; // init next pointer
+    head->prev = NULL;                  // init prev pointer
+    head->next = NULL;                  // init next pointer
 
     for (i = 0; i < MAX; i++) // since head is created, 5 more elements will created and added to listed list (wondering if I should change...)
     {
         item = malloc(sizeof(Node)); // allocate memory
         nr = get_random_number();    // get random number
         item->number = nr;           // assign number to item
-        head = add_first(head, get_random_number()); // temporary solution
+        item->next = NULL;           // init values
+        item->prev = NULL;           // init values
+        if (head->next == NULL)
+        {
+            head->next = item; // if head doesn't a node, set it to item
+            item->prev = head; // set head to previous of item
+        }
+        item->prev = old; // set previous of item to old
+        old->next = item; // set next to item
+        old = item;       // set old to item
     }
 
     return head;
 }
-
-/** 
-*  That adds a new record to the first position of the list and assign the field numbers the value of data.
-*  The function must return a pointer to the new first entry in the list. 
-*  Extend main() so that this function is tested.
-**/
 
 Node *add_first(Node *temp, int data)
 {
