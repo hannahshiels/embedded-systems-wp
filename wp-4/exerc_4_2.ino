94037b5b3e986ec8dfbd8ee2c6c1588dea87f69a
Hannah Shiels <thebelfastflygon@gmail.com>
2022-02-16 11:13:36 +0100

    add comments, separate functions, add comments

--- wp-4/exerc_4_2.ino
+++ wp-4/exerc_4_2.ino
@@ -11,15 +11,18 @@
 // use servo library
 #include <Servo.h>
 
+// pins for servos
+#define SERVO_S_PIN A0
+#define SERVO_M_PIN A1
+
 // declare variables
 Servo servo_s; // servo for seconds
 Servo servo_m; // servo for minutes
-#define SERVO_S_PIN A0
-#define SERVO_M_PIN A1
+
 int sDegrees = 0; // degrees for seconds servo
 int mDegrees = 0; // degrees for minutes servo
-int seconds = 0;
-int minutes = 0;
+int seconds = 0;  // measure number of seconds
+int minutes = 0;  // measure number of minutes
 
 void setup()
 {
@@ -34,7 +37,7 @@ void loop()
     reset_degrees();                   // if degrees are 180 then reset the position of servos
     increment_degrees_seconds_servo(); // increment number of degrees to seconds servo
     print_output();                    // print current seconds and minutes
-    update_seconds();                  // increment number of seconds and update minutes if need be
+    check_seconds();                   // increment number of seconds and update minutes if need be
     delay(1000);                       // wait 1 second
 }
 
@@ -42,16 +45,16 @@ void check_seconds() // check the seconds
 {
     if (seconds == 60) // if the seconds are 60
     {
-        seconds = 0;                       // reset number of seconds
-        minutes++;                         // increment number of minutes
-        increment_degrees_minutes_servo(); // increment minute servo
+        seconds = 0;         // reset number of seconds
+        increment_minutes(); // update number of minutes and degrees of minute servo
     }
+    seconds++; // increment seconds
 }
 
-void update_seconds()
+void increment_minutes()
 {
-    check_seconds(); // check if seconds need to be reset and update minutes if necessary
-    seconds++;       // increment seconds
+    minutes++;                         // increment number of minutes
+    increment_degrees_minutes_servo(); // increment minute servo
 }
 
 void reset_degrees()
@@ -79,8 +82,9 @@ void increment_degrees_minutes_servo() // increment the number of degrees for th
 
 void print_output()
 {
-    Serial.print(minutes); // print minutes
-    Serial.print(":");     // print spacer
-    Serial.print(seconds); // print seconds
-    Serial.println();      // ensure next line is on a new line
+    Serial.print(minutes);       // print minutes
+    Serial.print(" Minute(s) "); // print minutes message
+    Serial.print(seconds);       // print seconds
+    Serial.print(" Second(s)");  // print seconds message
+    Serial.println();            // ensure next line is on a new line
 }
